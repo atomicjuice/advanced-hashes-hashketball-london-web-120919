b418015277674bf901a94c62061fcea82bc97818
keemo <keemowatson1@gmail.com>
2019-12-10 06:59:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,23 @@
 # Write your code here!
 
 def game_hash
-  {:home => {:teamname},{:colors},{:players}}
+  {:home => 
+    {:team_name => "Charlotte Hornets"},
+    {:colors => ["Turquoise, Purple"]},
+    {:players => [
+      {:player_name => "Jeff Adrien",
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
+        }]}
+
+
+
+
   {:away => {:teamname},{:colors},{:players}}
 end
