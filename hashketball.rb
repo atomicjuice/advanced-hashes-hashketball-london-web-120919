102e1e68c8b114455ba0833fd4264eb00ff52a6e
keemo <keemowatson1@gmail.com>
2019-12-10 07:40:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -55,9 +55,9 @@ def game_hash
         :steals => 7,
         :blocks => 5,
         :slam_dunks => 12},
-        ]}}
+        ]}
 
-  {:home =>
+  :home =>
     {:team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
     :players => [
