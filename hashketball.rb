bdbb1b9e52797f63cb1e1a76bb0c08780c7679a4
keemo <keemowatson1@gmail.com>
2019-12-10 06:46:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,8 @@
 # Write your code here!
 
-
-
-
-
-
-
-
-
-
+def game_hash
+  {}
+  {:home => {teamname: "Brooklyn Nets"}}
+  {:away => {teamname: "Charlotte Hornets"}}
+  puts :home.fetch(teamname:)
+end
