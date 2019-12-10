7dc2592de5ef5883e63267a574d2c8baaebf23e8
keemo <keemowatson1@gmail.com>
2019-12-10 07:19:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     {:team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
     :players => [
-      :player_name => "Alan Anderson"
+      {:player_name => "Alan Anderson"
       :number => 0,
       :shoe => 16,
       :points => 22,
@@ -14,7 +14,7 @@ def game_hash
       :steals => 3,
       :blocks => 1,
       :slam_dunks => 1
-      }]}
+      }]}}
 
 
 
